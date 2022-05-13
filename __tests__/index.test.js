a92162783b3f9bf5cc9599871a9a34a291f79745
DimaKichigin <dimakichigin94@gmail.com>
2022-05-13 14:32:20 +0500

    MODIFIED __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,19 +9,20 @@ const __dirname = dirname(__filename);
 const fullPath = (file) => path.resolve(__dirname, '..', '__fixtures__', file);
 const data = (file) => readFileSync(fullPath(file), 'utf8');
 
-const filepath1 = fullPath('file1.json');
-const filepath2 = fullPath('file2.json');
+// const filepath1 = fullPath('file1.json');
+// const filepath2 = fullPath('file2.json');
+// const ymlFile1 = fullPath('file3.yml');
+// const ymlFile2 = fullPath('file4.yml');
+// const expection1 = data('expection1.txt');
 
-const ymlFile1 = fullPath('file3.yml');
-const ymlFile2 = fullPath('file4.yml');
-
-const expection1 = data('expection1.txt');
-// const expection2 = ('expection2.txt');
+const filepath1 = fullPath('file5.json');
+const filepath2 = fullPath('file6.json');
+const expection2 = ('expection2.txt');
 
 test('correctness of comparing flat json files', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(expection1);
+  expect(genDiff(filepath1, filepath2)).toEqual(expection2);
 });
 
-test('flat yml file comparison', () => {
-  expect(genDiff(ymlFile1, ymlFile2)).toEqual(expection1);
-});
+// test('flat yml file comparison', () => {
+//   expect(genDiff(ymlFile1, ymlFile2)).toEqual(expection1);
+// });
